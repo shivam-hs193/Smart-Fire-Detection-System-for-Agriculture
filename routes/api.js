60e89cb09b8d54c079b7fd628bb7a81c1d4d60c4
Shivam Harsh <127359345+shivam-hs193@users.noreply.github.com>
2025-06-06 01:31:08 +0530

    Delete routes/api.js

--- routes/api.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { logFireData } = require('../controllers/fireController');
-
-router.post('/log', logFireData);
-
-module.exports = router;
